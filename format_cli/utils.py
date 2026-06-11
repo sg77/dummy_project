271b6c1a19579b9500b6ebfbafcee76638fb8da7
Sebastian H. Guevara <sebastianhoracio.guevara@asml.com>
2026-06-11 19:22:57 +0200

    Add complexity

--- format_cli/utils.py
+++ format_cli/utils.py
@@ -1,9 +1,31 @@
 # random helpers
 
-def normalize_keys(d):
-    # TODO: recursive
-    return {k.lower(): v for k, v in d.items()}
+def normalize_keys(data):
+    if not isinstance(data, dict):
+        return data
 
+    def _process(items):
+        result = {}
+        for k, v in items:
+            new_k = str(k).lower().strip()
+            if isinstance(v, dict):
+                result[new_k] = _process(v.items())
+            elif isinstance(v, list):
+                temp = []
+                for x in v:
+                    if isinstance(x, dict):
+                        temp.append(_process(x.items()))
+                    else:
+                        temp.append(x)
+                result[new_k] = temp
+            else:
+                result[new_k] = v
+        return result
+
+    fn = lambda x: _process(x.items())
+    return fn(data)
+
+# TODO: simplify above mess
 
 class DebugMixin:
     def debug(self, msg):
